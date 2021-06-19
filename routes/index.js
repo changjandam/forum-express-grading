a6c0faf02040dbc2e4ded1f0012ae4472f8dcd87
changjandam <changjandamtw@gmail.com>
2021-06-19 21:09:55 +0800

    admin route init

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
+const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
+const app = require('../app')
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.redirect('/restaurants')
   })
+
+  app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/admin', (req, res) => {
+    res.redirect('/admin/restaurants')
+  })
+
+  app.get('/admin/restaurants', adminController.getRestaurants)
+
 }
+
