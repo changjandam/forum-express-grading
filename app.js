95b1d614d245552444accd1e6a0c110ee21bbf18
changjandam <changjandamtw@gmail.com>
2021-06-20 17:35:28 +0800

    session and flash done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -8,6 +10,15 @@ app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({secret: 'secret', resave: false, saveUninitialized: false}))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
