4f8b3fd254081b920d867e2873354751453dd3e7
changjandam <changjandamtw@gmail.com>
2021-06-27 23:02:13 +0800

    Revert "add delete comment func"
    This reverts commit bf657e8cfa1c0ee646bc6084f44ed261ffe1c5e7.

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -11,16 +11,6 @@ const commentController = {
       .then((comment) => {
         res.redirect(`/restaurants/${req.body.restaurantId}`)
       })
-  },
-
-  deleteComment: (req, res) => {
-    return Comment.findByPk(req.params.id)
-      .then((comment) => {
-        comment.destroy()
-          .then((comment) => {
-            res.redirect(`/restaurants/${comment.RestaurantId}`)
-          })
-      })
   }
 }
 
