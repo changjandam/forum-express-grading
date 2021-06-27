bf657e8cfa1c0ee646bc6084f44ed261ffe1c5e7
changjandam <changjandamtw@gmail.com>
2021-06-27 22:47:23 +0800

    add delete comment func

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,8 @@ module.exports = (app, passport) => {
   app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
 
   app.post('/comments', authenticated, commentController.postComment)
+  app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
