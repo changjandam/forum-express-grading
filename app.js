eed32bd23bafc06f913db6d237d56413cacc9e74
changjandam <changjandamtw@gmail.com>
2021-06-27 17:22:10 +0800

    cate func Read done

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config
 }
 
-app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', handlebars({ 
+  defaultLayout: 'main', 
+  extname: '.hbs',
+  helpers: require('./config/handlebars-helpers')
+}))
 app.set('view engine', 'hbs')
 
 app.use('/upload', express.static(__dirname+'/upload'))
