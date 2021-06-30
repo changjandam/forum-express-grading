0667b95fb0d813dedc72dff13fb960c74634276c
changjandam <changjandamtw@gmail.com>
2021-06-30 21:49:54 +0800

    try to fix db bug

--- migrations/20210623070659-add-inAdmin-to-users.js
+++ migrations/20210623070659-add-inAdmin-to-users.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // await queryInterface.addColumn('Users', 'isAdmin', {
-    //   type: Sequelize.BOOLEAN,
-    //   defaultValue: false,
-    // })
+    await queryInterface.addColumn('Users', 'isAdmin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    // await queryInterface.removeColumn('Users', 'isAdmin')
+    await queryInterface.removeColumn('Users', 'isAdmin')
   }
 };
