2c76099c9a52603e1de25bbdc5ab23cce3a6f2ea
changjandam <changjandamtw@gmail.com>
2021-06-30 16:05:23 +0800

    comments seed done

---
+++ seeders/20210630074339-comments-seed-file.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let comments = []
+
+for (let i = 0; i < 10; i++) {
+  comments.push({
+    text: `test${i}`,
+    UserId: 1,
+    RestaurantId: Math.floor(Math.random() * 50) * 10 + 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  })
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Comments', comments, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Comments', null, {})
+  }
+};
